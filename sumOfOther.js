e6d9672e4a34b32dd77b169df8e337890cc636c3
OLGA NARANOVICH <naranovicholga@gmail.com>
2018-10-28 00:32:26 +0300

    feat: function "sumOfOther"

---
+++ sumOfOther.js
@@ -0,0 +1,8 @@
+module.exports = function sumOfOther(arr) {
+	const resultSum = [];
+	for (let i = 0, len = arr.length; i < len; i++) {
+		let sum = (arr.reduce(function(a, b) {return a + b;}) - arr[i]);
+		resultSum.push(sum);
+	}
+	return resultSum;
+} 
